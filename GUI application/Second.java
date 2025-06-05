a55eb05688ac89d84bf58bb17f5590c8aba20ea6
Arpan Adhikari <adhikariarpan2063@gmail.com>
2025-06-05 15:38:20 +0545

    Color Changer

---
+++ GUI application/Second.java
@@ -0,0 +1,36 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Second extends JFrame implements ActionListener {
+
+    JTextField t;
+    JButton btn;
+
+    public Second() {
+        setTitle("Java");
+        setSize(400, 500);
+        t = new JTextField();
+        t.setColumns(10);
+        btn = new JButton("Click");
+        add(t);
+        add(btn);
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        btn.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        t.setBackground(Color.BLACK);
+        Font fnt = new Font("Arial", Font.BOLD, 20);
+        t.setFont(fnt);
+        t.setForeground(Color.RED);
+    }
+
+    public static void main(String[] args) {
+        new Second();
+    }
+}
