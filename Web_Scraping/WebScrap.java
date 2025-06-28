a0d6b2999de4c65e6e829108fbf503370f094f89
Arpan Adhikari <adhikariarpan2063@gmail.com>
2025-06-28 23:34:36 +0545

    Web Scraping

---
+++ Web_Scraping/WebScrap.java
@@ -0,0 +1,16 @@
+package URLConnection;
+import java.io.*;
+import java.net.*;
+
+public class WebScrap {
+    public static void main(String[] args) throws Exception{
+        URL url = new URL("https://ncit.edu.np/");
+        URLConnection cn = url.openConnection();
+        InputStream in = cn.getInputStream();
+        int data;
+        while ((data = in.read()) != -1) {
+            System.out.print((char) data);
+        }
+        in.close();
+    }
+}
